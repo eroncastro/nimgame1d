9946867161a10bf3a5edaa7077b28cd29e37f813
Eron Castro <eroncastro@gmail.com>
2017-01-13 20:34:08 -0200

    Fixes nim.py

--- nim.py
+++ nim.py
@@ -1,9 +1,9 @@
 def computador_escolhe_jogada(n, m):
     for i in range(m):
-        if (n - (i + 1)) % m == 0:
+        if (n - (i + 1)) % (m + 1) == 0:
             print('O computador retirou %s peça(s).' % (i + 1))
-            if (n - i + 1) > 0:
-                print('Agora restam %s peças no tabuleiro.' % (n - i + 1))
+            if (n - (i + 1)) > 0:
+                print('Agora restam %s peças no tabuleiro.' % (n - (i + 1)))
             else:
                 print('Fim do jogo! O computador ganhou!')
             return i + 1
@@ -13,12 +13,15 @@ def usuario_escolhe_jogada(n, m):
         jogadas_usuario = int(input('Quantas peças você vai tirar? '))
         if jogadas_usuario > m:
             print('Numero inválido de jogadas')
-        print('Você tirou %s peça(s)' % jogadas_usuario)
+        if n - jogadas_usuario > 0:
+            print('Você tirou %s peça(s)' % jogadas_usuario)
+        else:
+            print('Fim do jogo! Você ganhou!')
         return jogadas_usuario
 
 def partida():
     n = int(input('Quantas peças? '))
-    m = int(input('Limite de peças por jogada?'))
+    m = int(input('Limite de peças por jogada? '))
 
     if (n % (m + 1)) != 0:
         print('Computador começa!')
@@ -28,7 +31,12 @@ def partida():
 
     while n > 0:
         n = n - usuario_escolhe_jogada(n, m)
+        if n == 0:
+            break
+
         n = n - computador_escolhe_jogada(n, m)
+        if n == 0:
+            break
 
 print('Bem-vindo ao jogo do NIM! Escolha:')
 print('1 - para jogar uma partida isolada')
